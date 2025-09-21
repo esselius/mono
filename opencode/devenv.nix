4a99e38b8eb4f810c3fab16e41274f8b7e57cc69
Peter Esselius <pepp@me.com>
2025-09-21 21:05:20 +0200

    opencode config

--- opencode/devenv.nix
+++ opencode/devenv.nix
@@ -1,10 +1,20 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, config, ... }:
 
 let
   pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+  opencode-config = "${config.devenv.dotfile}/opencode.json";
 in
 {
+  env = {
+    OPENCODE_DISABLE_LSP_DOWNLOAD = "true";
+    OPENCODE_CONFIG = opencode-config;
+  };
+
   packages = with pkgs-unstable; [
     opencode
   ];
+
+  files.${opencode-config}.json = {
+    "$schema" = "https://opencode.ai/config.json";
+  };
 }
